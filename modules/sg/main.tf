9214b857c6b6e8a01e4689f74ba1bdf3b2897f3b
decaloms <sebastien.decalom@amersports.com>
2025-10-30 12:19:44 +0100

    module security group

---
+++ modules/sg/main.tf
@@ -0,0 +1,39 @@
+ #volume 8Gib on zone us-east-1a with tag ebs-volume
+ resource "aws_security_group" "security_group" {
+    name = var.security_group_name
+    description = "Rules for the module mini terraform"
+
+    # Trafic entrant (Ingress)
+    ingress {
+        description = "Allow HTTP"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow HTTPS"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        description = "Allow SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    # Trafic sortant (Egress)
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"       # -1 = tous les protocoles
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = var.security_group_tag
+ }
---
+++ modules/sg/variables.tf
@@ -0,0 +1,12 @@
+variable "security_group_name" {
+    type = string
+    default = "security_group"
+    description = "Name of the security group"
+}
+variable "security_group_tag" {
+  type = map
+  default = {
+    Name = "security_group"
+  }
+  description = "The tags for the security group"
+}
